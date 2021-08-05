88151c7d7d1ba47cae4427b5d2a11f055c32c828
seo-rio <scs9310@gmail.com>
2021-08-06 00:36:47 +0900

    실패율 완료

--- src/level/one/failurerate/Solution.java
+++ src/level/one/failurerate/Solution.java
@@ -2,11 +2,10 @@ package level.one.failurerate;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 /**
  * @실패율 https://programmers.co.kr/learn/courses/30/lessons/42889
@@ -14,38 +13,39 @@ import java.util.Map;
 public class Solution {
 
     private static final int N = 5;
-    private static final int[] stages = {2, 1, 2, 6, 2, 4, 3, 3};
-
+    private static final int[] stages = {2,1,2,4,2,4,3,3};
     public static void main(String[] args) {
         System.out.println(Arrays.toString(new Solution().solution(N, stages)));
     }
 
     public int[] solution(int N, int[] stages) {
-        int[] answer = {};
 
-        double[] resultArr = new double[N];
         Map<Integer, Double> failMap = new HashMap<>();
-        for(int i=1; i<=N; i++) {
+        for (int i = 1; i <= N; i++) {
             System.out.println("I =>" + i);
             int failCount = 0;
             int reachCount = 0;
             for (int stage : stages) {
-                if(i <= stage) {
+                if (i <= stage) {
                     reachCount++;
                 }
-                if(i == stage) {
+                if (i == stage) {
                     failCount++;
                 }
             }
-            double result = ((double) failCount/ (double) reachCount);
-            System.out.println(i + "번째 스테이지 실패율 => " + result);
+            double result;
+            if(failCount == 0 && reachCount == 0) {
+                result = 0;
+            }else{
+                result = ((double) failCount / (double) reachCount);
+            }
             failMap.put(i, result);
-            resultArr[i-1] = result;
         }
+        List<Integer> listKeySet = new ArrayList<>(failMap.keySet());
 
-        System.out.println(Arrays.toString(resultArr));
-        System.out.println(failMap);
+        listKeySet.sort((value1, value2) -> (failMap.get(value2).compareTo(failMap.get(value1))));
+        List<Integer> answerList = new ArrayList<>(listKeySet);
 
-        return answer;
+        return answerList.stream().mapToInt(i->i).toArray();
     }
 }
