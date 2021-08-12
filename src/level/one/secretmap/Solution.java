d9dd6316151a92f8f390578a0713b28ef462ebfe
seo-rio <scs9310@gmail.com>
2021-08-12 23:55:05 +0900

    비밀지도 풀이중

---
+++ src/level/one/secretmap/Solution.java
@@ -0,0 +1,20 @@
+package level.one.secretmap;
+
+/**
+ * @비밀지도 https://programmers.co.kr/learn/courses/30/lessons/17681
+ */
+public class Solution {
+
+    int n = 5;
+    int[] arr1 = {9, 20, 28, 18, 11};
+    int[] arr2 = {30, 1, 21, 17, 18};
+
+    public static void main(String[] args) {
+
+    }
+
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+        return answer;
+    }
+}
