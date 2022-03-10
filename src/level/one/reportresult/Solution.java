cead9cfaee89ae5a9923574c915ceeb6cd515886
chungsik-seo <724584@carrot-i.com>
2022-03-10 19:17:48 +0900

    신고 결과 받기 완료

--- src/level/one/reportresult/Solution.java
+++ src/level/one/reportresult/Solution.java
@@ -2,18 +2,19 @@ package level.one.reportresult;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
-import javafx.print.Collation;
 
 /**
  * @신고_결과_받기 https://programmers.co.kr/learn/courses/30/lessons/92334?language=java
  */
 public class Solution {
 
+//    static final String[] id_list = {"con", "ryan"};
     static final String[] id_list = {"muzi", "frodo", "apeach", "neo"};
+//    static final String[] report = {"ryan con", "ryan con", "ryan con", "ryan con"};
     static final String[] report = {"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"};
     static final int k = 2;
 
@@ -22,54 +23,62 @@ public class Solution {
     }
 
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = {};
+        int[] answer = new int[id_list.length];
 
         // <신고자 ID, 신고자가 신고한사람 목록> Map
-        LinkedHashMap<String, ArrayList<String>> reporterMap = new LinkedHashMap<>();
+        LinkedHashMap<String, HashSet<String>> reporterMap = new LinkedHashMap<>();
         for (String p : id_list) {
-            reporterMap.put(p, new ArrayList<>());
+            reporterMap.put(p, new HashSet<>());
         }
 
         // <신고자 ID, 신고자가 신고한사람 목록> Map 세팅
         reporterMap.keySet().forEach(r -> {
-            reporterMap.put(r, (ArrayList<String>) Arrays.stream(report)
+            reporterMap.put(r, (HashSet<String>) Arrays.stream(report)
                 .filter(p -> p.substring(0, p.indexOf(" ")).equals(r))
                 .map(rp -> rp.substring(rp.indexOf(" ") + 1))
-                .collect(Collectors.toList()));
+                .collect(Collectors.toSet()));
         });
 
-        reporterMap.keySet().forEach(r -> {
-            for(String data : reporterMap.get(r)) {
-                System.out.println(r + " : " + data);
-            }
+        ArrayList<String> reportedPersonList = new ArrayList<>();
+        reporterMap.forEach((key, value) -> {
+            reportedPersonList.addAll(value);
         });
-
-
-        // 신고된 사용자만 추출
-        String[] reportedPerson = Arrays.stream(report).map(p -> p.substring(p.indexOf(" ") + 1)).toArray(String[]::new);
         LinkedHashMap<String, Integer> reportPersonCountMap = new LinkedHashMap<>();
 
         for (String p : id_list) {
             reportPersonCountMap.put(p, 0);
         }
 
-        // 정지된 사람 목록
+        // 정지된 사람 개수 목록
         ArrayList<String> suspendedPersonList = new ArrayList<>();
 
-        reportPersonCountMap.keySet().forEach(p -> reportPersonCountMap.put(p, (int) Arrays.stream(reportedPerson).filter(rp -> rp.equals(p)).count()));
-        reportPersonCountMap.keySet().forEach(p -> {
-            if(reportPersonCountMap.get(p) >= k) {
-                System.out.println(p);
-                System.out.println(reportPersonCountMap.get(p));
-                suspendedPersonList.add(p);
+        reportPersonCountMap.keySet().forEach(p -> reportPersonCountMap.put(p, (int) reportedPersonList.stream().filter(rp -> rp.equals(p)).count()));
+        reportPersonCountMap.forEach((key, value) -> {
+            if(value >= k) {
+                suspendedPersonList.add(key);
             }
         });
 
         // TODO: 신고한 사람 중에 정지된 사람이 몇명이나 있는지 확인하여야함
-        reporterMap.keySet().forEach(r -> {
+        AtomicInteger index = new AtomicInteger();
+        reporterMap.forEach((key, value) -> {
+            AtomicInteger count = new AtomicInteger();
+            value.forEach(rp -> {
+                suspendedPersonList.forEach(sp -> {
+                    if(sp.equals(rp)) {
+                        count.getAndIncrement();
+                    }
+                });
+                answer[index.intValue()] = count.intValue();
+            });
+            index.getAndIncrement();
         });
 
+        for(int i : answer) {
+            System.out.println(i);
+        }
 
-        return Arrays.stream(id_list).mapToInt(reportPersonCountMap::get).toArray();
+
+        return answer;
     }
 }
