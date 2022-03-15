bc56b5d55b740e96394680727fd92b53323c5249
seo-rio <scs9310@gmail.com>
2022-03-15 18:50:08 +0900

    비밀 지도 완료

--- src/level/one/secretmap/Solution.java
+++ src/level/one/secretmap/Solution.java
@@ -5,16 +5,24 @@ package level.one.secretmap;
  */
 public class Solution {
 
-    int n = 5;
-    int[] arr1 = {9, 20, 28, 18, 11};
-    int[] arr2 = {30, 1, 21, 17, 18};
+//    static final int n = 5;
+    static final int n = 6;
+//    static final int[] arr1 = {9, 20, 28, 18, 11};
+    static final int[] arr1 = {46, 33, 33 ,22, 31, 50};
+//    static final int[] arr2 = {30, 1, 21, 17, 18};
+    static final int[] arr2 = {27 ,56, 19, 14, 14, 10};
 
     public static void main(String[] args) {
-
+        new Solution().solution(n, arr1, arr2);
     }
 
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
+        String[] answer = new String[n];
+        for(int i=0; i<n; i++) {
+            String result = String.format("%0" + n + "d", Long.parseLong(Integer.toBinaryString(arr1[i] | arr2[i])));
+            answer[i] = result.replaceAll("1", "#").replaceAll("0", " ");
+        }
         return answer;
     }
+
 }
